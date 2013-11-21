93bc7db2502f03a7aae1616fa0c53c36df757826
Casey Bisson <casey.bisson@gmail.com>
2013-11-20 18:03:24 -0800

    first commit of version 2
    This compiles cleanly, and I've remapped all the pins, but this is completely untested.

--- shutterfingers.ino
+++ shutterfingers.ino
@@ -1,33 +1,79 @@
-// shutterfingers
-//
-// Positions a servo to depress the shutter button of a camera when triggered
-// from a remote shutter release cable. See URL below for pinouts:
-// http://www.doc-diy.net/photo/eos_wired_remote/
+/*
+shutterfingers
 
-#include <Servo.h>
+Positions a servo to depress the shutter button of a camera when triggered
+from a remote shutter release cable. See URL below for pinouts:
+http://www.doc-diy.net/photo/eos_wired_remote/
 
-// trigger inputs, corresponding servo positions, and states
-const int shutter_pin = 2;  // pin on which the shutter signal is received
+This code is written for ATtiny85 running on a Digispark board. 
+See http://digistump.com/products/1
+
+Version 1 of the project can was written for a more common ATmega328 
+or Atmega32U4-powered Arduino environment. It's still available from:
+https://github.com/misterbisson/shutterfingers/releases
+
+The ATtiny85 differs in a number if ways from ATmega328 and Atmega32U4 environments.
+These two pages offer detail some of those differences:
+https://learn.sparkfun.com/tutorials/tiny-avr-programmer-hookup-guide/attiny85-use-hints
+http://digistump.com/wiki/digispark/tutorials/basics
+
+Regarding pins and pull-up (pullup) resistors on the Digispark and ATtiny85:
+  
+Quoting from http://digistump.com/wiki/digispark/tutorials/basics :
+
+  "Pin 3 (P3) has a 1.5 kΩ pull-up resistor attached to it which is required for 
+  when P3 and P4 are used for USB communication (including programming). Your design 
+  may need to take into account that you'd have to overpower this to pull this pin low."
+
+  "The internal pull-up resistor are much weaker (about 25 kohm) on an ATtiny 
+  than on an Arduino, so the onboard LED interferes with them. If you need them, 
+  you can use a different port. Change your circuit to not need the internal 
+  pull-up, or cut the LED trace."
+
+And because I think there are some typos in the above description about using 
+the the internal pull-ups, I used the example in http://digistump.com/board/index.php/topic,1086.msg4553.html#msg4553
+when implementing them in this project.
+*/
+
+/*
+Trigger inputs, corresponding servo positions, and states
+*/
+const int shutter_pin = 1;  // pin on which the shutter signal is received
 const int shutter_pos = 65; // position at which the camera's shutter is released
 int shutter_state;          // status of the shutter signal
-const int focus_pin   = 4;
+const int focus_pin   = 2;
 const int focus_pos   = 55;
 int focus_state;
 const int safety_pos  = 45;  // safety position for the servo, away from the shutter button
 
-// shutter servo details
-Servo shutter_servo;              // create servo object to control a servo
-const int shutter_servo_pin = 8;  // pin on which the servo is attached
+/*
+Shutter servo details
+Using Simple Servo here:
+http://digistump.com/board/index.php/topic,444.0.html
+https://github.com/digistump/DigisparkArduinoIntegration/tree/master/libraries/DigisparkSimpleServo
+
+Alternatives (untested) include:
+https://github.com/digistump/DigisparkArduinoIntegration/tree/master/libraries/DigisparkSoftRcPulseOut
+https://github.com/fri000/Servo8Bit
+*/
+#include <SimpleServo.h>
+SimpleServo shutter_servo;        // create servo object to control a servo
+const int shutter_servo_pin = 4;  // pin on which the servo is attached
 int shutter_servo_state;          // current state/position of the shutter servo
 
-const int led_pin_external     = 6;   // to help with debugging
-const int led_pin_internal     = 13;  // to help with debugging
+/*
+Gotta have some LEDs
+*/
+const int led_pin_external = 3;   // to help with debugging
+const int led_pin_internal = 3;   // to help with debugging
 
 void setup()
 {
   shutter_servo.attach( shutter_servo_pin );  // attaches the servo on the defined pin to the servo object
-  pinMode( shutter_pin, INPUT_PULLUP );
-  pinMode( focus_pin, INPUT_PULLUP );
+  pinMode( shutter_pin, INPUT );
+  digitalWrite( shutter_pin, HIGH ); // activate internal pull-up
+  pinMode( focus_pin, INPUT );
+  digitalWrite( shutter_pin, HIGH ); // activate internal pull-up
   pinMode( led_pin_internal, OUTPUT );
   pinMode( led_pin_external, OUTPUT );
 }
