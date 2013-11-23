ec4d4fdceb125db6a8f9ecb343e415466d33c359
Casey Bisson <casey.bisson@gmail.com>
2013-11-22 21:08:13 -0800

    added pin usage notes
    ...and moved the servo from 5 to 0 because pin 5 is supposedly "useless as an output"

--- shutterfingers.ino
+++ shutterfingers.ino
@@ -17,9 +17,24 @@ These two pages offer detail some of those differences:
 https://learn.sparkfun.com/tutorials/tiny-avr-programmer-hookup-guide/attiny85-use-hints
 http://digistump.com/wiki/digispark/tutorials/basics
 
-Regarding pins and pull-up (pullup) resistors on the Digispark and ATtiny85:
+Regarding pins usage and pull-up (pullup) resistors on the Digispark and ATtiny85:
 
-Quoting from http://digistump.com/wiki/digispark/tutorials/basics :
+Overview of pins, fromhttp://digistump.com/board/index.php/topic,986.0.html
+
+	"P5 has a weak drive, current is limited to a few mA, high level voltage is 3V @1mA and 2.5V @2mA
+	P4 is connected to a zener diode for USB, the internal pullup is too weak to set the pin to HIGH
+	P3 has a 1.5KΩ pullup
+	P0 or P1 (depending on model) is connected to the onboard led, which interferes with the internal pullup"
+
+More about pins to avoid, from http://digistump.com/board/index.php/topic,1105.0.html
+
+	"I was saying that P5 is weak and useless as an output because 
+	it's the reset pin and it's said in the datasheet you can't get a lot 
+	of current from it, voltage drops a lot when you drain even a few mAs."
+
+	"if you want to keep USB feature, P3/P4 must be kept"
+
+And quoting from http://digistump.com/wiki/digispark/tutorials/basics :
 
 	"Pin 3 (P3) has a 1.5 kΩ pull-up resistor attached to it which is required for
 	when P3 and P4 are used for USB communication (including programming). Your design
@@ -38,7 +53,7 @@ when implementing them in this project.
 /*
 Trigger inputs, corresponding servo positions, and states
 */
-const int shutter_pin = 0;  // pin on which the shutter signal is received
+const int shutter_pin = 5;  // pin on which the shutter signal is received
 const int shutter_pos = 65; // position at which the camera's shutter is released
 int shutter_state;          // status of the shutter signal
 const int focus_pin   = 2;
@@ -58,7 +73,7 @@ https://github.com/fri000/Servo8Bit
 */
 #include <SimpleServo.h>
 SimpleServo shutter_servo;        // create servo object to control a servo
-const int shutter_servo_pin = 5;  // pin on which the servo is attached
+const int shutter_servo_pin = 0;  // pin on which the servo is attached
 int shutter_servo_state;          // current state/position of the shutter servo
 
 /*
@@ -74,6 +89,7 @@ void setup()
 	pinMode( focus_pin, INPUT );
 	digitalWrite( shutter_pin, HIGH ); // activate internal pull-up
 	pinMode( led_pin, OUTPUT );
+	digitalWrite( led_pin, LOW );
 }
 
 void loop()
